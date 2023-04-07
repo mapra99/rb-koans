9bfb1753c30710a8f0c078fc1f665885e9906bc5
Miguel Prada <mapra99@gmail.com>
2023-04-07 07:52:51 -0500

    part 1

--- triangle.rb
+++ triangle.rb
@@ -13,8 +13,32 @@
 # and
 #   about_triangle_project_2.rb
 #
+
+def any_zero_side?(a, b, c)
+  [a, b, c].any?(:zero?)
+end
+
+def any_negative_side?(a, b, c)
+  [a, b, c].any?(:negative?)
+end
+
+def hicks_sum_smaller_than_hypothenuse?(a, b, c)
+  hick_1, hick_2, hypothenuse = [a, b, c].sort
+  hick_1 + hick_2 <= hypothenuse
+end
+
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  raise TriangleError if any_zero_side?(a, b, c)
+  raise TriangleError if any_negative_side?(a, b, c)
+  raise TriangleError if hicks_sum_smaller_than_hypothenuse?(a, b, c)
+
+  if a == b && b == c
+    :equilateral
+  elsif a == b || a == c || b == c
+    :isosceles
+  else
+    :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
